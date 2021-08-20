1e253ddd230adcde9446a9dfcd967f0b24b64bc2
DiscreteTom <discrete_tom@outlook.com>
2021-08-20 23:03:52 +0800

    remove old example, add lockstep websocket

---
+++ example/lockstep-websocket-broadcast/README.md
@@ -0,0 +1,6 @@
+# Lockstep Websocket Broadcast Example
+
+The websocket server will be started on `:8080`.
+
+It's recommended to use [wscat](https://www.npmjs.com/package/wscat) as a client to test this example.
+
---
+++ example/lockstep-websocket-broadcast/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"DiscreteTom/rua/pkg/lockstep"
+	"DiscreteTom/rua/pkg/model"
+	"DiscreteTom/rua/plugin/network/websocket"
+	"fmt"
+	"log"
+)
+
+func main() {
+	errChan := make(chan error)
+	s := lockstep.NewLockStepServer().SetHandleKeyboardInterrupt(true)
+
+	ws := websocket.NewWebsocketListener(":8080", s)
+	go func() {
+		errChan <- ws.Start()
+	}()
+
+	serverErrsChan := make(chan []error)
+	go func() {
+		serverErrsChan <- s.Start(broadcastStepHandler)
+	}()
+
+	select {
+	case err := <-errChan:
+		log.Println(err)
+	case errs := <-serverErrsChan:
+		if len(errs) != 0 {
+			log.Println(errs)
+		}
+		break
+	}
+}
+
+func broadcastStepHandler(step int, peers map[int]model.Peer, commands map[int][]byte, _ *lockstep.LockstepServer) (errs []error) {
+	// compact commands in one byte array
+	msg := []byte(fmt.Sprintf("step: %d\n", step))
+	for id, m := range commands {
+		msg = append(msg, []byte(fmt.Sprintf("from %d:\n", id))...)
+		msg = append(msg, m...)
+		msg = append(msg, '\n')
+	}
+	// broadcast to everyone
+	for _, p := range peers {
+		go p.Write(msg)
+	}
+	return
+}
