fa9e62e84043c585c36f88fa3c6ac0f9c509ff37
DiscreteTom <discrete_tom@outlook.com>
2021-08-21 11:49:24 +0800

    update kcp listener, add more attrs

--- plugin/network/kcp/listener.go
+++ plugin/network/kcp/listener.go
@@ -8,25 +8,46 @@ import (
 )
 
 type kcpListener struct {
-	addr    string
-	gs      model.GameServer
-	key     []byte
-	bufSize int
+	addr         string
+	gs           model.GameServer
+	key          []byte
+	bufSize      int
+	dataShards   int
+	parityShards int
+	crypt        string
 }
 
 func NewKcpListener(addr string, gs model.GameServer, key []byte, bufSize int) *kcpListener {
 	return &kcpListener{
-		addr:    addr,
-		gs:      gs,
-		key:     key,
-		bufSize: bufSize,
+		addr:         addr,
+		gs:           gs,
+		key:          key,
+		bufSize:      bufSize,
+		dataShards:   10,
+		parityShards: 3,
+		crypt:        "aes",
 	}
+}
+
+func (l *kcpListener) WithDataShards(shards int) *kcpListener {
+	l.dataShards = shards
+	return l
+}
+
+func (l *kcpListener) WithParityShards(shards int) *kcpListener {
+	l.parityShards = shards
+	return l
+}
+
+func (l *kcpListener) WithCrypt(crypt string) *kcpListener {
+	l.crypt = crypt
+	return l
 }
 
 func (l *kcpListener) Start() error {
 	log.Println("kcp server is listening at", l.addr)
-	block, _ := kcp.NewAESBlockCrypt(l.key)
-	listener, err := kcp.ListenWithOptions(l.addr, block, 10, 3)
+	block, _ := blockCrypt(l.crypt, l.key)
+	listener, err := kcp.ListenWithOptions(l.addr, block, l.dataShards, l.parityShards)
 	if err != nil {
 		return err
 	}
@@ -37,5 +58,37 @@ func (l *kcpListener) Start() error {
 		}
 		p := &kcpPeer{c: c, gs: l.gs, bufSize: l.bufSize}
 		l.gs.AddPeer(p)
+	}
+}
+
+func blockCrypt(crypt string, key []byte) (kcp.BlockCrypt, error) {
+	switch crypt {
+	case "sm4":
+		return kcp.NewSM4BlockCrypt(key[:16])
+	case "tea":
+		return kcp.NewTEABlockCrypt(key[:16])
+	case "xor":
+		return kcp.NewSimpleXORBlockCrypt(key)
+	case "none":
+		return kcp.NewNoneBlockCrypt(key)
+	case "aes-128":
+		return kcp.NewAESBlockCrypt(key[:16])
+	case "aes-192":
+		return kcp.NewAESBlockCrypt(key[:24])
+	case "blowfish":
+		return kcp.NewBlowfishBlockCrypt(key)
+	case "twofish":
+		return kcp.NewTwofishBlockCrypt(key)
+	case "cast5":
+		return kcp.NewCast5BlockCrypt(key[:16])
+	case "3des":
+		return kcp.NewTripleDESBlockCrypt(key[:24])
+	case "xtea":
+		return kcp.NewXTEABlockCrypt(key[:16])
+	case "salsa20":
+		return kcp.NewSalsa20BlockCrypt(key)
+	default:
+		log.Println("unknown cryption, use aes")
+		return kcp.NewAESBlockCrypt(key)
 	}
 }
