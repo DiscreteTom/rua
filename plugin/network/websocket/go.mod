564dfff54ad031b06d1494494375d5162f12b3f8
DiscreteTom <discrete_tom@outlook.com>
2021-08-26 15:49:25 +0800

    update websocket plugin to adopt BasicPeer

--- plugin/network/websocket/go.mod
+++ plugin/network/websocket/go.mod
@@ -3,6 +3,6 @@ module github.com/DiscreteTom/rua/plugin/network/websocket
 go 1.17
 
 require (
-	github.com/DiscreteTom/rua v0.0.0-20210824193227-e5d0277b8907
+	github.com/DiscreteTom/rua v0.0.0-20210826072452-060ec44a1892
 	github.com/gorilla/websocket v1.4.2
 )
--- plugin/network/websocket/peer.go
+++ plugin/network/websocket/peer.go
@@ -8,85 +8,42 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type websocketPeer struct {
-	id     int // peer id
-	c      *websocket.Conn
-	gs     rua.GameServer
-	lock   sync.Mutex
-	closed bool
-	tag    string
-	logger rua.Logger
-}
+func NewWebsocketPeer(c *websocket.Conn, gs rua.GameServer) *rua.BasicPeer {
+	lock := sync.Mutex{}
+	closed := false
 
-func NewWebsocketPeer(c *websocket.Conn, gs rua.GameServer) *websocketPeer {
-	return &websocketPeer{
-		c:      c,
-		gs:     gs,
-		lock:   sync.Mutex{},
-		closed: false,
-		tag:    "websocket",
-		logger: rua.GetDefaultLogger(),
-	}
-}
+	return rua.NewBasicPeer(gs).
+		WithTag("websocket").
+		OnWrite(func(data []byte, p *rua.BasicPeer) error {
+			// prevent concurrent write
+			lock.Lock()
+			defer lock.Unlock()
 
-func (p *websocketPeer) WithLogger(l rua.Logger) *websocketPeer {
-	p.logger = l
-	return p
-}
+			return c.WriteMessage(websocket.BinaryMessage, data)
+		}).
+		OnClose(func(p *rua.BasicPeer) error {
+			// wait for write finished
+			lock.Lock()
+			defer lock.Unlock()
 
-func (p *websocketPeer) WithTag(t string) *websocketPeer {
-	p.tag = t
-	return p
-}
-
-func (p *websocketPeer) SetTag(t string) {
-	p.tag = t
-}
-
-func (p *websocketPeer) GetTag() string {
-	return p.tag
-}
-
-func (p *websocketPeer) Activate(id int) {
-	p.id = id
-}
-
-// Thread safe.
-func (p *websocketPeer) Write(data []byte) error {
-	// prevent concurrent write
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	return p.c.WriteMessage(websocket.BinaryMessage, data)
-}
-
-func (p *websocketPeer) Close() error {
-	// wait for write finished
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	p.closed = true
-	return p.c.Close() // close websocket conn
-}
-
-func (p *websocketPeer) GetId() int {
-	return p.id
-}
-
-func (p *websocketPeer) Start() {
-	for {
-		_, msg, err := p.c.ReadMessage()
-		if err != nil {
-			if !p.closed {
-				// not closed by Close(), we should remove the peer
-				p.logger.Error(err)
-				if err := p.gs.RemovePeer(p.id); err != nil {
-					p.logger.Error(err)
+			closed = true
+			return c.Close() // close websocket conn
+		}).
+		OnStart(func(p *rua.BasicPeer) {
+			for {
+				_, msg, err := c.ReadMessage()
+				if err != nil {
+					if !closed {
+						// not closed by Close(), we should remove the peer
+						p.GetLogger().Error(err)
+						if err := p.GetGameServer().RemovePeer(p.GetId()); err != nil {
+							p.GetLogger().Error(err)
+						}
+					}
+					break
 				}
-			}
-			break
-		}
 
-		p.gs.AppendPeerMsg(p.id, msg)
-	}
+				p.GetGameServer().AppendPeerMsg(p.GetId(), msg)
+			}
+		})
 }
