30a88718febb2ad63a3dd6e8531ded153de17556
DiscreteTom <discrete_tom@outlook.com>
2021-09-11 00:50:35 +0800

    Update StdioPeer using SafePeer

--- peer/debug/stdio.go
+++ peer/debug/stdio.go
@@ -4,53 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sync"
 
 	"github.com/DiscreteTom/rua"
 	"github.com/DiscreteTom/rua/peer"
 )
 
 type StdioPeer struct {
-	peer.BasicPeer
-	lock *sync.Mutex
+	*peer.SafePeer
 }
 
 func NewStdioPeer(gs rua.GameServer) (*StdioPeer, error) {
-	p := &StdioPeer{lock: &sync.Mutex{}}
+	p := &StdioPeer{}
 
-	bp, err := peer.NewBasicPeer(
+	sp, err := peer.NewSafePeer(
 		gs,
 		peer.Tag("stdio"),
-		peer.OnWrite(func(data []byte, _ *peer.BasicPeer) error {
-			// prevent concurrent write
-			p.lock.Lock()
-			defer p.lock.Unlock()
-
-			_, err := fmt.Print(string(data))
-			return err
-		}),
-		peer.OnClose(func(_ *peer.BasicPeer) error {
-			// wait after write finished
-			p.lock.Lock()
-			defer p.lock.Unlock()
-
-			return nil
-		}),
-		peer.OnStart(func(_ *peer.BasicPeer) {
-			reader := bufio.NewReader(os.Stdin)
-			for {
-				line, err := reader.ReadString('\n')
-				if err != nil && err.Error() != "EOF" {
-					p.Logger().Error("rua.StdioReadString:", err)
-				}
-				p.GameServer().AppendPeerMsg(p.Id(), []byte(line))
-			}
-		}),
 	)
 	if err != nil {
 		return nil, err
 	}
+	sp.OnWriteSafe(func(data []byte) error {
+		_, err := fmt.Print(string(data))
+		return err
+	})
+	sp.OnCloseSafe(func() error {
+		return nil
+	})
+	sp.OnStart(func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil && err.Error() != "EOF" {
+				p.Logger().Error("rua.StdioReadString:", err)
+			}
+			p.GameServer().AppendPeerMsg(p.Id(), []byte(line))
+		}
+	})
 
-	p.BasicPeer = *bp
+	p.SafePeer = sp
 	return p, nil
 }
