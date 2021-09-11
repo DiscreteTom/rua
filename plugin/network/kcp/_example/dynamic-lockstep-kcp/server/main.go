694ff6812fb62913b3e6a2cd3f277b86a7a8ff5f
DiscreteTom <discrete_tom@outlook.com>
2021-09-11 14:23:43 +0800

    update kcp examples to adapt rua v0.4.0

--- plugin/network/kcp/_example/dynamic-lockstep-kcp/server/main.go
+++ plugin/network/kcp/_example/dynamic-lockstep-kcp/server/main.go
@@ -13,9 +13,8 @@ import (
 
 func main() {
 	errChan := make(chan error)
-	s := rua.NewLockstepServer().
-		SetHandleKeyboardInterrupt(true).
-		OnStep(dynamicStepHandler)
+	s := rua.NewLockstepServer()
+	s.OnStep(dynamicStepHandler(s))
 
 	key := pbkdf2.Key([]byte("demo pass"), []byte("demo salt"), 1024, 32, sha1.New)
 	go func() {
@@ -29,34 +28,37 @@ func main() {
 
 	select {
 	case err := <-errChan:
-		s.GetLogger().Error(err)
+		s.Logger().Error(err)
 	case errs := <-serverErrsChan:
 		if len(errs) != 0 {
-			s.GetLogger().Error(errs)
+			s.Logger().Error(errs)
 		}
 		break
 	}
 }
 
 // Change step length according to the 1st msg's latency.
-func dynamicStepHandler(msgs []rua.PeerMsg, s *rua.LockstepServer) {
-	if len(msgs) != 0 && len(msgs[0].Data) == 8 {
-		sendTime := int64(binary.LittleEndian.Uint64(msgs[0].Data))
-		recvTime := msgs[0].Time.UnixMilli()
-		rtt := int(recvTime - sendTime) // round trip time
+func dynamicStepHandler(s *rua.LockstepServer) func(msgs []rua.PeerMsg) {
+	return func(msgs []rua.PeerMsg) {
 
-		s.GetLogger().Infof("rtt(ms): %d", rtt)
-		s.SetStepLength(rtt)
-		s.GetLogger().Infof("new step length: %d", s.GetCurrentStepLength())
-	}
+		if len(msgs) != 0 && len(msgs[0].Data) == 8 {
+			sendTime := int64(binary.LittleEndian.Uint64(msgs[0].Data))
+			recvTime := msgs[0].Time.UnixMilli()
+			rtt := int(recvTime - sendTime) // round trip time
 
-	// broadcast current time
-	buf := make([]byte, 8)
-	currentTime := time.Now().UnixMilli()
-	binary.LittleEndian.PutUint64(buf, uint64(currentTime))
-	for _, p := range s.GetPeers() {
-		if err := p.Write(buf); err != nil {
-			s.GetLogger().Error(err)
+			s.Logger().Infof("rtt(ms): %d", rtt)
+			s.WithStepLength(rtt)
+			s.Logger().Infof("new step length: %d", s.CurrentStepLength())
 		}
+
+		// broadcast current time
+		buf := make([]byte, 8)
+		currentTime := time.Now().UnixMilli()
+		binary.LittleEndian.PutUint64(buf, uint64(currentTime))
+		s.ForEachPeer(func(id int, peer rua.Peer) {
+			if err := peer.Write(buf); err != nil {
+				s.Logger().Error(err)
+			}
+		})
 	}
 }
