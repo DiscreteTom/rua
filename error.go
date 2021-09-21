c2b74a4a8ae13633475476a92478962bad2519e0
DiscreteTom <discrete_tom@outlook.com>
2021-09-21 13:59:53 +0800

    add ErrPeerWriteTimeout

--- error.go
+++ error.go
@@ -3,6 +3,7 @@ package rua
 import "errors"
 
 var (
-	ErrPeerClosed   = errors.New("peer already closed")
-	ErrPeerNotExist = errors.New("peer not exist")
+	ErrPeerClosed       = errors.New("peer already closed")
+	ErrPeerNotExist     = errors.New("peer not exist")
+	ErrPeerWriteTimeout = errors.New("peer write timeout")
 )
