8f0b95c75c68ecaab3eacdff5029671f51425e89
DiscreteTom <discrete_tom@outlook.com>
2021-10-30 22:27:36 +0800

    update example/file-persistent

--- example/file-persistent/main.go
+++ example/file-persistent/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/DiscreteTom/rua"
-	"github.com/DiscreteTom/rua/peer/debug"
-	"github.com/DiscreteTom/rua/peer/persistent"
 )
 
 func main() {
-	s := rua.NewEventDrivenServer()
-	s.OnPeerMsg(func(msg *rua.PeerMsg) {
-		s.ForEachPeer(func(peer rua.Peer) {
-			if peer.Tag() == "file" {
-				peer.Write(msg.Data)
-			}
-		})
-	})
+	file, err := rua.DefaultFileNode().Filename("log.txt").Go()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	s.AddPeer(debug.NewStdioPeer(s))
-	s.AddPeer(persistent.NewFilePeer("./log.txt", s))
+	stdio := rua.DefaultStdioNode().OnMsg(func(b []byte) {
+		file.Write(b)
+	}).Go()
 
-	s.Start()
+	rua.NewCtrlc().OnSignal(func() {
+		file.Stop()
+		stdio.Stop()
+	}).Wait()
 }
