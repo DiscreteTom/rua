0e24bcb7894d9cec88e134406b639bd9bc00160a
DiscreteTom <discrete_tom@outlook.com>
2021-10-30 22:27:43 +0800

    remove unused files

--- error.go
+++
@@ -1,9 +0,0 @@
-package rua
-
-import "errors"
-
-var (
-	ErrPeerClosed       = errors.New("peer already closed")
-	ErrPeerNotExist     = errors.New("peer not exist")
-	ErrPeerWriteTimeout = errors.New("peer write timeout")
-)
--- utils.go
+++
@@ -1,7 +0,0 @@
-package rua
-
-func WriteOrLog(p Peer, msg []byte) {
-	if err := p.Write(msg); err != nil {
-		p.Logger().Errorf("rua.WriteOrLog.[id=%d,tag=%s]: %s", p.Id(), p.Tag(), err)
-	}
-}
