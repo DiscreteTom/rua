9bd708da7b5812a01d6087e506aadf3234f73502
DiscreteTom <discrete_tom@outlook.com>
2021-11-16 23:32:17 +0800

    remove ws cascader

--- plugin/network/websocket/cascade-leader.go
+++
@@ -1,9 +0,0 @@
-package websocket
-
-import (
-	"github.com/DiscreteTom/rua"
-)
-
-func NewWebsocketCascadeLeader(addr string, gs rua.GameServer) *websocketListener {
-	return NewWebsocketListener(addr, gs).WithPeerTag("websocket/cascade/leader")
-}
