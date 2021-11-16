454ec06740f549a5b88fd9fae7f958bc0885ecfe
DiscreteTom <discrete_tom@outlook.com>
2021-11-16 23:42:07 +0800

    optimize comments

--- plugin/network/websocket/_example/ws-broadcast/main.go
+++ plugin/network/websocket/_example/ws-broadcast/main.go
@@ -7,16 +7,22 @@ import (
 
 // Use `wscat -c ws://127.0.0.1:8080` to connect to the websocket server.
 func main() {
+	// create broadcaster
 	bc := rua.NewBroadcaster()
 
+	// websocket listener
 	ws, _ := websocket.NewWsListener("127.0.0.1:8080").OnNewPeer(func(wn *websocket.WsNode) {
+		// add new peer to the broadcaster
 		bc.AddTarget(wn.OnMsg(func(b []byte) {
+			// new message will be write to the broadcaster
 			bc.Write(b)
 		}).Go())
-	}).Go()
+	}).Go() // start the listener
 
+	// also broadcast to stdout
 	bc.AddTarget(rua.DefaultStdioNode().Go())
 
+	// wait for ctrl-c
 	rua.NewCtrlc().OnSignal(func() {
 		bc.StopAll()
 		ws.Stop()
